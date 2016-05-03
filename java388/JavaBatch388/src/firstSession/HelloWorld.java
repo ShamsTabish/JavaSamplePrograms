2a83f3329687053fc24d3ca08a634891db0c911e
Shaikh <ssm2349@I.local>
2016-05-03 20:36:54 +0530

    Added Main Method, Variables, DataTypes, TypeCasting, operators

---
+++ java388/JavaBatch388/src/firstSession/HelloWorld.java
@@ -0,0 +1,19 @@
+package firstSession;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 Comment Example - C-Style
+		 */
+		
+		/*
+		 * Java Documentation Comment
+		 * 
+		 */
+		System.out.println("Hello World!!!");
+		
+	}
+
+}
---
+++ java388/JavaBatch388/src/firstSession/OperatorsExample.java
@@ -0,0 +1,16 @@
+package firstSession;
+
+public class OperatorsExample {
+	public static void main(String[] args) {
+		int no1=34;
+		no1+=6; // no1 = no1 + 6;
+		
+		System.out.println("Value is "+no1);
+		
+		no1--; // no1 = no1 -1;
+		System.out.println("Value now is "+no1);
+		
+		
+	}
+
+}
